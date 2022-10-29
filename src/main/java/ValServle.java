04a1bf57455aceb2a40e485bd8a093cae4d5d140
rakshithabai <113327766+rakshithabai@users.noreply.github.com>
2022-10-29 18:26:09 +0530

    Initial Commit

---
+++ src/main/java/ValServle.java
@@ -0,0 +1,30 @@
+
+
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ValServle
+ */
+@WebServlet("/ValServle")
+public class ValServle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public void service(HttpServletRequest request, HttpServletResponse response)  
+		    throws ServletException, IOException {  
+		  
+		    response.setContentType("text/html");  
+		    PrintWriter out = response.getWriter();  
+		          
+		    String n=request.getParameter("Name");  
+		    out.print("Welcome "+n);  
+		    response.sendRedirect("Home.html"); 
+		          
+		    out.close();  
+		    }  
+		  
+		}  
